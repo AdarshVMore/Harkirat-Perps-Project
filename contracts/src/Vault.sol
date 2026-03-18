8afbbdfecb5ee8533b736e8977879c140c31b772
Adarsh-Ellenox <skullcrushermore@gmail.com>
2026-03-19 03:22:10 +0530

    small change test

--- contracts/src/Vault.sol
+++ contracts/src/Vault.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 contract Vault {
     mapping(address => uint256) public balances;
 
-    function deposit() external payable {
+    function depositMain() external payable {
         balances[msg.sender] += msg.value;
     }
 }
